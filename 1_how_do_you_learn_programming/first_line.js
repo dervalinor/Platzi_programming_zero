e524de58f3257c59df9e6f05146b7aa51483339b
dervalinor <dervalinor@protonmail.com>
2022-08-26 23:18:34 -0500

    first commit

---
+++ 1_how_do_you_learn_programming/first_line.js
@@ -0,0 +1,16 @@
+//You must use the browser and enter inspect-console and write
+a = 1; //now we created a variable, this are as box where we will store
+//data
+b = 2;//space storage in memory for data
+
+a + b;//the operations of variables are store in RAM
+
+//function - block of code for execute a task, inside a function 
+//is the parameter (value send to the function)
+//name_function(parameter);
+
+alert("Hello friend");//this a example of a function
+
+//text between quotes is string (chain caracters)
+
+//programming is control the flow of actions inside a computer
